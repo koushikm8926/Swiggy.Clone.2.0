3c5e2c593f577fa09b3943c1fe2ad3b6c37e3d23
Koushik Mondal <darkdeavil004@gmail.com>
2023-06-27 10:57:20 +0530

    Carousel Error Fixed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,39 @@
-import { StyleSheet, Text, View,SafeAreaView, Platform, TextInput } from 'react-native'
-import React from 'react'
-import { Feather } from '@expo/vector-icons';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  TextInput,
+} from "react-native";
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import Carousel from "../components/Carousel";
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={{paddingTop:Platform.OS==="android"?35:0,}}>
-      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center',borderColor:'#C0C0C0', borderWidth:1,padding:10, margin:10, borderRadius:7}}>
-        <TextInput placeholder='Search for Restaurent item or more' style={{fontSize:17, width:"90%",}}/>
+    <SafeAreaView style={{ paddingTop: Platform.OS === "android" ? 35 : 0 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          borderColor: "#C0C0C0",
+          borderWidth: 1,
+          padding: 10,
+          margin: 10,
+          borderRadius: 7,
+        }}
+      >
+        <TextInput
+          placeholder="Search for Restaurent item or more"
+          style={{ fontSize: 17, width: "90%" }}
+        />
         <Feather name="search" size={24} color="#E52B50" />
       </View>
+      <Carousel/>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
