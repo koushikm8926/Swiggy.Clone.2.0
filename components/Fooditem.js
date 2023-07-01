7f41e7edf1249d7f1a7d7b0a7f76f04b7ebd65f7
Koushik Mondal <darkdeavil004@gmail.com>
2023-07-01 19:30:06 +0530

    Recomended part mapped

--- components/Fooditem.js
+++ components/Fooditem.js
@@ -7,12 +7,19 @@ const Fooditem = ({item}) => {
     const data=[item];
     console.log(data);
     const [selected, setSelected]=useState([]);
-
+  const handleItemSelect = (item)=>{
+    const itemSelected = selected.find((c)=> c===item);
+    if (itemSelected){
+      setSelected(selected.filter((sel)=> sel !==item));
+    }else{
+      setSelected([...selected, item]);
+    }
+  }
   return (
     <View>
       {data.map((item,i)=>(
         <>
-        <Pressable key={i}style={{flexDirection:'row', alignItems:'center', justifyContent:"space-between", margin:10,}} >
+        <Pressable  onPress={()=>handleItemSelect(item.name)} key={i}style={{flexDirection:'row', alignItems:'center', justifyContent:"space-between", margin:10,}} >
             <Text style={{fontSize:20,fontWeight:'bold'}}>{item.name}</Text>
             <AntDesign name="down" size={24} color="black" />
         </Pressable>
